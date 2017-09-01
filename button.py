7cfdb12a63a853e54bf06db5ce6116f71a67abd9
Riyaz Ul Haque <30811072+RiyazUlHaque@users.noreply.github.com>
2017-09-01 07:29:50 -0700

    Add files via upload

---
+++ button.py
@@ -0,0 +1,15 @@
+#HOW TO MAKE BUTTON IN A WINDOW
+
+from tkinter import *
+ 
+root=Tk()
+
+#BUTTON CODE
+button1=Button(root,text="BUTTON-1",bg="lightgreen")
+button1.pack()
+
+#BUTTON CODE
+button2=Button(root,text="BUTTON-1",bg="lightblue")
+button2.pack()
+
+root.mainloop()
